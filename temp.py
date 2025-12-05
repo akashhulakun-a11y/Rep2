96a2976c3bcc7efa681028e16468712ce6f14446
akashhulakun-a11y <akashhulakun@gmail.com>
2025-12-05 12:30:15 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,8 @@
+temperature = float(input("Enter temperature: "))
+
+if temperature >= 30:
+    print("Weather is Hot!")
+elif temperature >= 20:
+    print("Weather is Normal.")
+else:
+    print("Weather is Cold!")
